c19dfe0cdd9069733cc1123e46f42294bdb1f5bf
Tim Irwin <tirwin@iot.in.gov>
2015-11-11 10:47:48 -0500

    Wed at 10:45

--- Client/app/notes/notes.js
+++ Client/app/notes/notes.js
@@ -10,27 +10,34 @@
 
       .state('notes', {
         url: '/notes',
+        resolve: {
+          notesLoaded: ['NotesService',function(NotesService) {
+            return NotesService.fetch();
+          }]
+        },
         templateUrl: '/notes/notes.html',
         controller: NotesController
       })
 
       .state('notes.form', {
-          url: '/:noteId',
-          templateUrl: '/notes/notes-form.html'
+        url: '/:noteId',
+        templateUrl: '/notes/notes-form.html',
+        controller: NotesFormController
       });
   }
 
-  NotesController['$inject'] = ['$state', '$scope', 'NotesService'];
+  NotesController.$inject = ['$state', '$scope', 'NotesService'];
   function NotesController($state, $scope, NotesService) {
     $scope.note = {};
+    $scope.notes = NotesService.get();
+  }
+
+  NotesFormController.$inject = ['$scope', '$state', 'NotesService'];
+  function NotesFormController($scope, $state, NotesService) {
+    $scope.note = NotesService.findById($state.params.noteId);
 
     $scope.save = function() {
       NotesService.save($scope.note);
     };
-
-    NotesService.fetch().then(function() {
-      $scope.notes = NotesService.get();
-    });
-    $state.go('notes.form');
   }
 })();
--- Client/app/services/notes-service.js
+++ Client/app/services/notes-service.js
@@ -27,6 +27,16 @@ function NotesService($http) {
     return self.notes;
   };
 
+  self.findById = function(noteId) {
+    // Look through `self.notes` for a note with a matching _id.
+    for (var i = 0; i < self.notes.length; i++) {
+      if (self.notes[i]._id === noteId) {
+        return angular.copy (self.notes[i]);
+      }
+    }
+    return {};
+  };
+
   self.save = function(note) {
     $http.post('http://localhost:3000/notes', {
       note: note
