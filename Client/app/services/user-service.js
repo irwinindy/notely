33db248abb3d72a8259888f7a7124c6e51d999cd
Tim Irwin <tirwin@iot.in.gov>
2015-11-12 16:57:22 -0500

    end of day

--- Client/app/services/user-service.js
+++ Client/app/services/user-service.js
@@ -1,16 +1,20 @@
 angular.module('notely')
-.service('UsersService', ['$http', 'API_BASE', ($http, API_BASE) =>{
-  class  UsersService {
-      create(user){
+  .service('UsersService', [
+    '$http', 'API_BASE', 'AuthToken', 'CurrentUser',
+     ($http, API_BASE, AuthToken, CurrentUser) => {
+
+    class UsersService {
+      create(user) {
         let userPromise = $http.post(`${API_BASE}users`, {
           user: user
         });
         userPromise.then((response) => {
-          console.log(response.data.user)
+          AuthToken.set(response.data.auth_token);
+          CurrentUser.set(response.data.user);
         });
         return userPromise;
       }
     }
     return new UsersService();
-  
-}]);
+
+  }]);
--- server/routes/notes.js
+++ server/routes/notes.js
@@ -3,7 +3,7 @@ var Note = require('../models/note');
 
 // List all notes
 router.get('/', function(req, res) {
-  Note.find().sort({ updated_at: -1 }).then(function(notes) {
+  Note.find({ user: req.user }).sort({ updated_at: -1 }).then(function(notes) {
     res.json(notes);
   });
 });
@@ -12,7 +12,8 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
   var note = new Note({
     title: req.body.note.title,
-    body_html: req.body.note.body_html
+    body_html: req.body.note.body_html,
+    user: req.user
   });
 
   note.save().then(function(noteData) {
@@ -25,7 +26,7 @@ router.post('/', function(req, res) {
 
 // Update an existing note
 router.put('/:id', function(req, res) {
-  Note.findOne({ _id: req.params.id }).then(function(note) {
+  Note.findOne({ _id: req.params.id, user: req.user }).then(function(note) {
     note.title = req.body.note.title;
     note.body_html = req.body.note.body_html;
     note.save().then(function() {
@@ -38,7 +39,7 @@ router.put('/:id', function(req, res) {
 });
 
 router.delete('/:id', function(req, res) {
-  Note.findOne({ _id: req.params.id }).then(function(note) {
+  Note.findOne({ _id: req.params.id, user: req.user }).then(function(note) {
     note.remove().then(function() {
       res.json({
         message: 'That note has been deleted.',
